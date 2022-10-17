9398ffb0212889bcec60ea84cd3c6d2d7e69c572
devsilva <jersonsilva99@hotmail.com>
2022-10-17 15:36:19 -0500

    remove: Remove react-redux

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,11 @@ import App from './App'
 import ReactDOM from 'react-dom/client'
 
 import { $ } from './utils/dom'
-import { Provider } from 'react-redux'
 
 import './index.css'
 
 ReactDOM.createRoot($('#root')).render(
   <React.StrictMode>
-    <Provider>
       <App />
-    </Provider>
   </React.StrictMode>
 )
