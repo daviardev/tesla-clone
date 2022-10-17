cf4b8fa5fe79cd5964d5725638ff275742bf60da
devsilva <jersonsilva99@hotmail.com>
2022-10-17 15:35:52 -0500

    feat: Add component Home and import Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import Header from './components/Header'
+import Home from './components/Home'
 import './App.css'
 
 const App = () => {
 
   return (
     <div className="App">
-      
+      { /* <Header /> */ }
+      <Home />
     </div>
   )
 }
