cfdb5ad5ffab7e46457565a10f1eb2b815164c8c
devsilva <jersonsilva99@hotmail.com>
2022-10-19 16:24:09 -0500

    refactor: Import redux in the main file

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import App from './App'
 import ReactDOM from 'react-dom/client'
 
+import { store } from '../app/store'
+
 import { $ } from './utils/dom'
+import { Provider } from 'react-redux'
 
 import './index.css'
 
 ReactDOM.createRoot($('#root')).render(
   <React.StrictMode>
+    <Provider store={store}>
       <App />
+    </Provider>
   </React.StrictMode>
 )
